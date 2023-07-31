e3fa070b988646d852afe12d8f8afd538a5f6115
CyntiaRambu <cyntiaverolita@gmail.com>
2023-07-31 15:16:40 +0700

    Saucedemo

---
+++ Object Repository/Saucedemo/Product/btn_Addtocart.rs
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Addtocart</name>
+   <tag></tag>
+   <elementGuidId>29560944-4dad-4963-9591-4529f846c956</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'btn btn_primary btn_small btn_inventory' and (text() = 'Add to cart' or . = 'Add to cart')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn_primary btn_small btn_inventory</value>
+      <webElementGuid>7037d815-f268-49e5-b089-90ef0eda83e3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Add to cart</value>
+      <webElementGuid>b580f8c6-de53-47e9-8ef0-bad6d52a1697</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Saucedemo/Product/btn_reset.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_reset</name>
+   <tag></tag>
+   <elementGuidId>7564f3da-e73c-4bdd-90dd-f2540fe24cb1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#reset_sidebar_link</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
